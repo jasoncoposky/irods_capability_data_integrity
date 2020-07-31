93def1238468a6abfbf0438fb28c20c0353de6e1
d-w-moore <dmoore@renci.org>
2020-07-31 05:45:18 -0400

    add verify_checksum.py

---
+++ irods_capability_integrity_utils.py
@@ -0,0 +1,14 @@
+
+def get_error_value           ()   : return "ERROR_VALUE"
+def RULE_ENGINE_CONTINUE      ()   : return 5000000
+def SYS_INVALID_INPUT_PARAM   ()   : return -130000
+def verify_replicas_attribute ()   : return "irods::verification::replica_placement"
+def verify_checksum_attribute ()   : return "irods::verification::checksum"
+def true(): return "true"
+
+def split_text_lines( string_in ) :
+    return filter (None, string_in.split("\n"))
+
+def join_text_lines( list_in ) :
+    return "\n".join(filter(None, list_in)) + "\n"
+
