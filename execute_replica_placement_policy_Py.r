2a4934b4e7835e2746747aaac399e6ca33db7dea
d-w-moore <dmoore@renci.org>
2020-07-31 07:03:46 -0400

    Update execute_replica_placement_policy_Py.r

--- execute_replica_placement_policy_Py.r
+++ execute_replica_placement_policy_Py.r
@@ -2,7 +2,7 @@ execute_replica_placement_policy {
     from irods_capability_integrity_utils import (split_text_lines, )
     violations = ""
 
-    callback.verify_replica_placement(violations)
+    retval = callback.verify_replica_placement(violations)
     violations = retval ['arguments'][0]
     for v in split_text_lines(violations):
         callback.writeLine("stdout", v)
