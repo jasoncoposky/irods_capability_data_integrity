f880f9ddc7eb7fdd16d1b611f8d0394c161da09a
d-w-moore <dmoore@renci.org>
2020-07-31 06:21:55 -0400

    add verify_replica_number.py

--- irods_capability_integrity_utils.py
+++ irods_capability_integrity_utils.py
@@ -4,6 +4,8 @@ def RULE_ENGINE_CONTINUE      ()   : return 5000000
 def SYS_INVALID_INPUT_PARAM   ()   : return -130000
 def verify_replicas_attribute ()   : return "irods::verification::replica_placement"
 def verify_checksum_attribute ()   : return "irods::verification::checksum"
+def verify_replica_number_attribute ()   : return "irods::verification::replica_number"
+
 def true(): return "true"
 
 def split_text_lines( string_in ) :
